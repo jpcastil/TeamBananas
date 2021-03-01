71bccc943442db6a3d6731d68f19c90bf6a71321
eflore53 <59632517+eflore53@users.noreply.github.com>
2021-03-01 12:11:27 -0800

    added js to title.html
    trying to access the elements that would change from getting the new info

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ function searchTitle() {
   var x;
   // Get the value of the input field with id="searchT"
   x = document.getElementById("searchT").value;
-  alert(x);
+  //alert(x);
 }
---
+++ title.js
@@ -0,0 +1,8 @@
+function loadTitle() {
+  var x;
+  // Get the value of the input field with id="searchT"
+  x = document.getElementById("filterName").innerHTML;
+  //var res = str.replace("MOVIe", "SOUL");
+  //document.getElementById("demo").innerHTML = res;
+  alert(x);
+}
