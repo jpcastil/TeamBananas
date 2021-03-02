339b9d905043eb1d1d7c430a93ebd1a784b5e1bf
eflore53 <59632517+eflore53@users.noreply.github.com>
2021-03-01 20:13:29 -0800

    added loadTitleInfo
    loadTitleInfo changes the html to correspond to the searched title's date, genre, and length

--- title.js
+++ title.js
@@ -1,10 +1,12 @@
-function loadTitle() {
+function loadTitleInfo() {
   var x;
   // Get the value of the input field with id="searchT"
   x = document.getElementById("filterName").innerHTML;
   var res = x.replace("MOVIE", "SOUL");
   document.getElementById("filterName").innerHTML = res;
   loadTitleName();
+  loadTitleRating();
+  loadTitleData();
 }
 
 function loadTitleName() {
@@ -13,4 +15,24 @@ function loadTitleName() {
   x = document.getElementsByClassName("title-name")[0].innerHTML;
   var res = x.replace("MOVIE", "SOUL");
   document.getElementsByClassName("title-name")[0].textContent = res;
+}
+
+function loadTitleRating() {
+  var x;
+  // Get the value of the input field with class="rating"
+  x = document.getElementsByClassName("rating")[0].innerHTML;
+  var res = x.replace("X", "PG");
+  document.getElementsByClassName("rating")[0].textContent = res;
+}
+
+function loadTitleData() {
+  var time = "1hr 47min";
+  var genre = "Drama/Animation";
+  var date = "25 December 2020";
+  var info = time + " | " + genre + " | " + date;
+  // Get the value of the input field with class="title-name"
+  var x = document.getElementsByClassName("title-data")[0].innerHTML;
+
+  //var res = x.replace("X", "PG");
+  document.getElementsByClassName("title-data")[0].textContent = info;
 }
