2cbfb871fcc08f9bde5250cbda1e514fb8f9696d
eflore53 <59632517+eflore53@users.noreply.github.com>
2021-03-01 19:37:00 -0800

    added loadTitleName()
    accessing and replacing html elements

--- title.js
+++ title.js
@@ -2,7 +2,15 @@ function loadTitle() {
   var x;
   // Get the value of the input field with id="searchT"
   x = document.getElementById("filterName").innerHTML;
-  //var res = str.replace("MOVIe", "SOUL");
-  //document.getElementById("demo").innerHTML = res;
-  alert(x);
+  var res = x.replace("MOVIE", "SOUL");
+  document.getElementById("filterName").innerHTML = res;
+  loadTitleName();
+}
+
+function loadTitleName() {
+  var x;
+  // Get the value of the input field with class="title-name"
+  x = document.getElementsByClassName("title-name")[0].innerHTML;
+  var res = x.replace("MOVIE", "SOUL");
+  document.getElementsByClassName("title-name")[0].textContent = res;
 }
